7c7fe1b91a50d8200b472a124fc4c07a1b9a4c18
Nureddin Furkan Çevik <72273576+NFurkanC@users.noreply.github.com>
2023-11-20 00:09:57 +0300

    Updated variable names

--- quiz2soru.c
+++ quiz2soru.c
@@ -3,21 +3,21 @@
 int main()
 {
 int sayi, taban, hedef_taban,basamak,sonuc;
-printf("sayi  ve donusturmek istediginiz hedef tabani giriniz\n ");
+printf("Sayiyi, tabani ve hedef tabani giriniz:\n ");
 scanf("%d %d %d",&sayi ,&taban, &hedef_taban);
 //onluğa dönüştür
-int a = 0,us = 0;
+int onluk = 0,us = 0;
 while(sayi != 0){
     basamak = sayi % 10;
-    a = a + (basamak * pow(taban, us));
+    onluk += (basamak * pow(taban, us));
     ++us;
     sayi /= 10;
 }
 printf("%d\n",a);
 //a = onluk sayi
-while(a>0){
- sonuc=a%hedef_taban;
- a/=hedef_taban;
+while(onluk>0){
+ sonuc=onluk%hedef_taban;
+ onluk/=hedef_taban;
  printf(" %d \n",sonuc);   
 }
 
